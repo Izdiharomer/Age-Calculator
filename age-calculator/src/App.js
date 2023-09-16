d6d4d15d1394d9577222897780410afe4a97f969
Izdihar <ezdiharomerrrr@gmail.com>
2023-09-16 15:57:42 +0300

    feat: validation added to the input

--- age-calculator/src/App.js
+++ age-calculator/src/App.js
@@ -1,34 +1,51 @@
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import React, { useState } from "react";
 
 function App() {
   const currentYear = 2023;
-  const [birthYear, setbirthYear] = useState(0);
+  const [birthYear, setBirthYear] = useState(0);
   const [age, setAge] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
 
   function clickHandler() {
-    const theAge = currentYear - birthYear;
-    setAge(theAge);
+    if (birthYear >= 2023 || birthYear <= 1900) {
+      setErrorMessage("Please enter a valid birth year.");
+      setAge(0);
+    } else {
+      const theAge = currentYear - birthYear;
+      setAge(theAge);
+      setErrorMessage("");
+    }
   }
 
   function inputChangeHandler(e) {
-    setbirthYear(e);
+    setBirthYear(e);
   }
+
   return (
     <div className="App">
       <header className="App-header">
-      <h1></h1>
-      <h2></h2>
-      <input onChange={(e) => inputChangeHandler(e.target.value)} />
-      <div>
-        {" "}
-        <button onClick={clickHandler}>Submit</button>
-      </div>
+        <h1></h1>
+        <h2></h2>
+        <input
+          className="form-control-sm"
+          id="inputPassword2"
+          onChange={(e) => inputChangeHandler(parseInt(e.target.value))}
+        />
+        <div>
+          <button
+            className="btn btn-primary btn-lg mx-3 px-5 py-3 mt-2"
+            onClick={clickHandler}
+          >
+            Submit
+          </button>
+        </div>
 
-      <div>The age is: {age}</div>
-        
+        {errorMessage && <span>{errorMessage}</span>}
+        {!errorMessage && age > 0 && (
+          <span>You are {age} years old</span>
+        )}
       </header>
     </div>
   );
