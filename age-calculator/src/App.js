0740f10cc469c493c2749723e9ebff0569cf9ca7
Izdihar <ezdiharomerrrr@gmail.com>
2023-09-16 15:41:45 +0300

    Feat: first function created

--- age-calculator/src/App.js
+++ age-calculator/src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState } from "react";
 
 function App() {
+  const currentYear = 2023;
+  const [birthYear, setbirthYear] = useState(0);
+  const [age, setAge] = useState(0);
+
+  function clickHandler() {
+    const theAge = currentYear - birthYear;
+    setAge(theAge);
+  }
+
+  function inputChangeHandler(e) {
+    setbirthYear(e);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        HI
+      <h1></h1>
+      <h2></h2>
+      <input onChange={(e) => inputChangeHandler(e.target.value)} />
+      <div>
+        {" "}
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+
+      <div>The age is: {age}</div>
+        
       </header>
     </div>
   );
