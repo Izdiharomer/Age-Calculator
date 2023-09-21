d877ca91e6d7d647605f692d9dfc820ab6e4d68f
Izdihar <ezdiharomerrrr@gmail.com>
2023-09-21 11:16:01 +0300

    update

--- age-calculator/src/index.js
+++ age-calculator/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Practice from './Practice';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Practice/>
     <App />
   </React.StrictMode>
 );
