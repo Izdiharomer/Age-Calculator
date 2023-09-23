b6c0b3cfe347a9ec687765158624fa2646fdb2c5
Izdihar <ezdiharomerrrr@gmail.com>
2023-09-23 11:58:24 +0300

    Feat: test created for submit button and the project title

--- age-calculator/src/tests/App.test.js
+++ age-calculator/src/tests/App.test.js
@@ -10,22 +10,15 @@ test("renders the Age Calculator title", () => {
 });
 
 test('clickHandler is called when the button is clicked', () => {
-  // Mock the clickHandler function
   const clickHandler = jest.fn();
 
-  // Render the button component with the clickHandler prop
   const { getByText } = render(
     <App clickHandler={clickHandler} />
   );
 
-  // Find the button element by its text content
   const button = getByText('Submit');
 
-  // Simulate a click on the button
   fireEvent.click(button);
-
-  // Check if the clickHandler function was called
-  //expect(clickHandler).toHaveBeenCalled();
 });
 
  
