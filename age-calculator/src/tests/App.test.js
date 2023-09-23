f683579f4fb2426b56571e9804650faa7c749d6a
Izdihar <ezdiharomerrrr@gmail.com>
2023-09-23 12:01:03 +0300

    Feat: test is created for reset button

--- age-calculator/src/tests/App.test.js
+++ age-calculator/src/tests/App.test.js
@@ -9,7 +9,7 @@ test("renders the Age Calculator title", () => {
   expect(titleElement).toBeInTheDocument();
 });
 
-test('clickHandler is called when the button is clicked', () => {
+test('clickHandler is called when the submit button is clicked', () => {
   const clickHandler = jest.fn();
 
   const { getByText } = render(
@@ -17,6 +17,18 @@ test('clickHandler is called when the button is clicked', () => {
   );
 
   const button = getByText('Submit');
+
+  fireEvent.click(button);
+});
+
+test('clickHandler is called when the reset button is clicked', () => {
+  const resetHandler = jest.fn();
+
+  const { getByText } = render(
+    <App resetHandler={resetHandler} />
+  );
+
+  const button = getByText('Reset');
 
   fireEvent.click(button);
 });
